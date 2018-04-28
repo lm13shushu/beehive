bca766a453eb4821fd9b42c916c0c01ba2131693
Your Name <742862703@qq.com>
2018-04-28 20:32:08 +0000

    part1-finish-change7

--- app/Observers/CommentObserver.php
+++ app/Observers/CommentObserver.php
@@ -14,7 +14,7 @@ class CommentObserver
      public function created(Comment $comment)
     {
         $microblog = $comment->microblog;
-        //$microblog->increment('reply_count', 1);
+        $microblog->increment('reply_count', 1);
 
         // 通知作者话题被回复了，自定义notify方法
         $microblog->user->notify(new MicroblogReplied($comment));
