26844769f625accbe6b672809964e60f53e7284b
Your Name <742862703@qq.com>
2018-04-28 20:12:36 +0000

    part1-finish-change2

--- app/Observers/CommentObserver.php
+++ app/Observers/CommentObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Comment;
+use App\Models\Microblog;
 use App\Notifications\MicroblogReplied;
 
 
@@ -14,7 +15,8 @@ class CommentObserver
      public function created(Comment $comment)
     {
         $microblog = $comment->microblog;
-        $microblog->increment('reply_count', 1);
+        $res=Microblog::class->where('id',$microblog->id)->increment('reply_count');
+
 
         // 通知作者话题被回复了，自定义notify方法
         $microblog->user->notify(new MicroblogReplied($comment));
